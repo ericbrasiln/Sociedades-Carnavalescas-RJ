df498e00b1f4ce159bacd3b621b73ecee88b7528
ericbrasiln <profericbrasil@unilab.edu.br>
2020-06-02 23:40:40 -0300

    data para csv to html

--- tratamento-csv.py
+++ tratamento-csv.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('/home/ebn/Documentos/GitHub/Sociedades-Carnavalescas-RJ/Sociedades Carnavalescas (1900-1914) - GIFI-AN.csv')
+df = pd.read_csv('/home/ebn/Documentos/GitHub/Sociedades-Carnavalescas-RJ/S_C.csv')
+
+df = df.drop(columns=['IMG', 'Transcrever'])
+
+df.to_csv('S_C2.csv', index=False)
