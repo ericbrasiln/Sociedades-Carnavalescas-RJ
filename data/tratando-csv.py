a2f94a6497c6ec0b7c6a339b23effcb51d721c97
ericbrasiln <profericbrasil@unilab.edu.br>
2020-06-06 00:46:49 -0300

    Delete tratando-csv.py

--- data/tratando-csv.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import csv
-
-df = pd.read_csv('/home/ebn/Documentos/GitHub/Sociedades-Carnavalescas-RJ/data/Sociedades Carnavalescas.csv')
-
-df.to_csv('SC-v2.csv', index=True)
